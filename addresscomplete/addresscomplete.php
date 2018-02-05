f100f395f51589efee8013770bb18a86a69a38e8
Elisa Foltyn <mail@coolcat-creations.com>
2018-02-05 10:10:36 +0100

    Update addresscomplete.php
    Add Licence Comment

--- addresscomplete/addresscomplete.php
+++ addresscomplete/addresscomplete.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Fields.Addresscomplete
+ *
+ * @copyright   Copyright (C) 2018 COOLCAT creations. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 defined('_JEXEC') or die;
 JLoader::import('components.com_fields.libraries.fieldsplugin', JPATH_ADMINISTRATOR);
 
